ef80197fd5dbd4372b3d2447a94ee652f54abb3c
bipin <vsinghvipin333@gmail.com>
2024-07-12 19:52:35 +0530

    Some error

--- server/src/index.js
+++ server/src/index.js
@@ -9,38 +9,45 @@ import { v2 as cloudinary } from "cloudinary";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Resolving dirname for ES module
+// Load environment variables
+dotenv.config();
 
+// Resolving dirname for ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+// Connect to database
 dbConnection();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json({ limit: "100mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 
+// API routes
 app.use("/api", userRoute);
 
-// Use the client app
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// Render client for any path
+// The "catch-all" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
-
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Application listening on PORT No. ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
